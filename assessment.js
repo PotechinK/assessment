a246b0052f0917584e9041c8922dc2f327e30782
PotechinK <zenzen.ok.no.problem@gmail.com>
2026-05-27 18:48:42 +0900

    Add files via upload

--- assessment.js
+++ assessment.js
@@ -16,15 +16,29 @@ assessmentButton.addEventListener( // イベント検知設定の追加
     
     // 診断結果表示のエリア作成
     resultDivision.innerText = ''; // divタグを空文字で上書きすることで、空にしている
-    const header = document.createElement('h3');
-    header.innerText = '診断結果';
-    resultDivision.appendChild(header);
+
+    // headerDivision の作成
+    const headerDivision = document.createElement('div');
+    headerDivision.setAttribute('class', 'card-header text-bg-primary');
+    headerDivision.innerText = '診断結果';
+
+    //bodyDivision の作成
+    const bodyDivision = document.createElement('div');
+    bodyDivision.setAttribute('class','card-body');
 
     const paragraph = document.createElement('p');
-    const result = assessment(userName);
+    paragraph.setAttribute('class','card-text');
+      const result = assessment(userName);
     paragraph.innerText = result;
-    resultDivision.appendChild(paragraph);
+    bodyDivision.appendChild(paragraph);
 
+    // resultDivision に Bootstrap のスタイルを適用する
+    resultDivision.setAttribute('class','card');
+
+    // headerDivision と bodyDivision を resultDivision に差し込む
+    resultDivision.appendChild(headerDivision);
+    resultDivision.appendChild(bodyDivision);
+    
     // X投稿ボタンの作成
     tweetDivision.innerText = ''; //Tweetのdivタグを空にする
     const anchor = document.createElement('a'); // aタグの作成
